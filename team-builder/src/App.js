b70fbd6623ce1e37c9a9ffad04c9b6d6dab3c9ef
Mosharraf Musa <mosharrafny@gmail.com>
2020-03-17 20:41:38 -0400

    Build the Form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from "./components/Form"
+import UserCard from "./components/UserCard"
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    role: ""
+  });
+  const onInputChange = event => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+    console.log(formData);
+  };
+  const [teamList] = useState([]);
+
+  const submitForm = event => {
+    teamList.push(formData);
+    setFormData({
+      fname: "",
+      lname: "",
+      email: "",
+      role: ""
+    });
+
+    event.preventDefault();
+
+    console.log(teamList);
+    console.log(formData);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Form
+        onInputChange={onInputChange}
+        formData={formData}
+        submitForm={submitForm}
+       />
       </header>
-    </div>
-  );
+      <h3>Team List</h3>
+      <div className="card-wrapper">
+        {teamList.map(a => {
+           return (
+            <UserCard
+              fname={a.fname}
+              lname={a.lname}
+              email={a.email}
+              role={a.role}
+            />
+          );
+        })}
+     </div>
+  </div>
+ );
 }
 
 export default App;
